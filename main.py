4233f81fb360d39af3d8fda681ff99a72b8bebf0
Po-Wei Cheng <a821023a821023@yahoo.com.tw>
2017-11-21 13:55:03 +0800

    .

--- main.py
+++ main.py
@@ -36,6 +36,7 @@ classes = [
 # The machine should know how to move and resize the box proposal to output bounding box, which boxes the object.
 import pandas as pd
 import numpy as np
+from comp_2 import *
 
 np.set_printoptions(edgeitems=10000)
 np.core.arrayprint._line_width = 10000
@@ -46,29 +47,6 @@ print(df_.head())
 
 # We can prepare our training data by transforming coordinates [x1, y1, x2, y2]
 # into [delta_x, delta_y, log(delta_w), log(delta_h)].
-
-
-def bbox_transform(ex_rois, gt_rois):
-
-    ex_widths = ex_rois[2] - ex_rois[0] + 1.0
-    ex_heights = ex_rois[3] - ex_rois[1] + 1.0
-    ex_ctr_x = ex_rois[0] + 0.5 * ex_widths
-    ex_ctr_y = ex_rois[1] + 0.5 * ex_heights
-
-    gt_widths = gt_rois[2] - gt_rois[0] + 1.0
-    gt_heights = gt_rois[3] - gt_rois[1] + 1.0
-    gt_ctr_x = gt_rois[0] + 0.5 * gt_widths
-    gt_ctr_y = gt_rois[1] + 0.5 * gt_heights
-
-    targets_dx = (gt_ctr_x - ex_ctr_x) / ex_widths
-    targets_dy = (gt_ctr_y - ex_ctr_y) / ex_heights
-    targets_dw = np.log(gt_widths / ex_widths)
-    targets_dh = np.log(gt_heights / ex_heights)
-
-    targets = np.array([targets_dx, targets_dy, targets_dw, targets_dh])
-    return targets
-
-
 # Here we also resize the images into fixed 500*300 since the size of images are not the same
 from PIL import Image
 
